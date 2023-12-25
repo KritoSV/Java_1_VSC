068b0c3088d226f6a89e15088d9585e9c92a9b4a
KaraRi <kirarichi51@gmail.com>
2023-12-25 10:51:43 +0200

    Начало ООП.

---
+++ Java_OOP/Sem_1/VendingMachine.java
@@ -0,0 +1,9 @@
+package Java_OOP.Sem_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface VendingMachine {
+    public void initProduct(List<Product> productList);
+    public Product getProduct(String name);
+}
